d3a12ef54f193f5c44f4f239d8a0da0a9aeafe58
Jeremie Litzler <jeremie.litzler@gmail.com>
2021-06-06 20:05:30 +0200

    feat: complete vue router basics

--- Real.world.vue2.course/events-app/src/App.vue
+++ Real.world.vue2.course/events-app/src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link :to="{ name: 'event-list' }">Events</router-link> |
+      <router-link :to="{ name: 'event-details' }">Event details</router-link> |
+      <router-link :to="{ name: 'event-create' }">About</router-link>
     </div>
     <router-view />
   </div>
--- Real.world.vue2.course/events-app/src/router/index.js
+++ Real.world.vue2.course/events-app/src/router/index.js
@@ -1,28 +1,31 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import EventList from '../views/EventList.vue';
+import EventCreate from '../views/EventCreate.vue';
+import EventDetails from '../views/EventDetails.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'event-list',
+    component: EventList,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate,
+  },
+  {
+    path: '/event',
+    name: 'event-details',
+    component: EventDetails,
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
 });
