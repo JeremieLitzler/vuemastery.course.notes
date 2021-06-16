1b38b50c03c964fb69315985d19fafd1f4cf48d0
Jeremie Litzler <jeremie.litzler@conduent.com>
2021-06-16 11:07:54 +0200

    init project for mastering vuex

---
+++ Mastering.Vuex/events-app/src/router/index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import EventList from '../views/EventList.vue';
+import EventCreate from '../views/EventCreate.vue';
+import EventDetails from '../views/EventDetails.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/',
+    name: 'event-list',
+    component: EventList,
+  },
+  {
+    path: '/event/create',
+    name: 'event-create',
+    component: EventCreate,
+  },
+  {
+    path: '/event/:id',
+    name: 'event-details',
+    component: EventDetails,
+    props: true,
+  },
+];
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes,
+});
+
+export default router;
